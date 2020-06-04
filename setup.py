fedec0539c7f7eef15577493c374bc2619d65edc
Samuel Ng <samuelngsh@gmail.com>
2020-06-04 13:31:12 -0700

    Updated Dockross build setup to work with latest SiQADConn configuration, updated setup.py with more project information.

--- setup.py
+++ setup.py
@@ -8,4 +8,23 @@ setup (
         cmake_with_sdist = True,
         packages = ['siqadtools'],
         zip_safe = False
+        author="Walus Lab",
+        author_email="siqaddev@gmail.com",
+        description="Python tools for the SiQAD project.",
+        url="https://github.com/siqad/siqadconn",
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "Programming Language :: C++",
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Topic :: Scientific/Engineering",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: POSIX :: Linux",
+            "Operating System :: Microsoft :: Windows",
+            ],
+        python_requires='>=3.5',
         )
