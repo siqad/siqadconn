6d7b72a7b7d2600d6d51869dc479ba78f3efe5c3
Samuel Ng <samuelngsh@gmail.com>
2020-06-04 13:41:31 -0700

    Appveyor tests 4

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup (
         version = '0.2.2',
         cmake_with_sdist = True,
         packages = ['siqadtools'],
-        zip_safe = False
-        author="Walus Lab",
-        author_email="siqaddev@gmail.com",
-        description="Python tools for the SiQAD project.",
-        url="https://github.com/siqad/siqadconn",
-        classifiers=[
+        zip_safe = False,
+        author = "Walus Lab",
+        author_email = "siqaddev@gmail.com",
+        description = "Python tools for the SiQAD project.",
+        url = "https://github.com/siqad/siqadconn",
+        classifiers = [
             "Intended Audience :: Science/Research",
             "Programming Language :: C++",
             "Programming Language :: Python :: 3 :: Only",
